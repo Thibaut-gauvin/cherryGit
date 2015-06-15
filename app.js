3f486c0014717832dcdc4920e24917c33f7f7ace
gauvin <gauvin.thibaut83@gmail.com>
2015-06-15 08:58:09 +0200

    command cd ok but have issue when try to checkut branch with git

--- app.js
+++ app.js
@@ -1,43 +1,75 @@
+require('shelljs/global');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser());
 var Twig = require('twig');
+
 
 /* application variable */
 var git_project_path = '/var/www';
+var listenPort = '8080';
 
+if (!which('git')) {
+    echo('Sorry, this script requires git');
+    exit(1);
+}
 
-/*
-app.get('/index/:message', function(req, res) {
-    res.render('index.twig', {
-        message : req.params.message
-    });
+/* ROUTES */
+
+/*app.get('/index', function (req, res) {
+    var view = 'index';
+    renderView(res, view);
 });
 
-// redirection si mauvaise url
-app.use(function(req, res, next){
-    res.redirect('/index/coucou');
+app.get('/postApi', function (req, res) {
+    var view = 'form';
+    renderView(res, view);
+});
+
+app.post('/postApi', function (req, res) {
+    var repositoryName = req.body.repository;
+    var branchName = req.body.branch;
+    var commitHAsh = req.body.commit;
+    var params = [repositoryName, branchName, commitHAsh];
+    goToRepository(params, res);
+});
+
+// bad Url
+app.use(function(req, res){
+    res.redirect('/index');
 });*/
 
-
-app.get('/repo/:repository', function (req, res, renderView) {
+app.get('/repo/:repository/branch/:branch/commit', function (req, res) {
     var repositoryName = req.params.repository;
-    goToRepository(repositoryName, res);
+    var branchName = req.params.branch;
+    var commitHAsh = 'ea72eaa1a6a9208ba8d3a1cfbed4f2dc5b9bda94';
+    var params = [repositoryName, branchName, commitHAsh];
+    goToRepository(params, res);
 });
 
+/* Function */
 
+function goToRepository(params, res) {
+    var path =  git_project_path + '/' + params[0];
+    console.log('cd ' + path + '\n');
 
-function goToRepository(repositoryName, res) {
-    var path =  git_project_path + '/' + repositoryName;
-    renderView(path, res);
-}
-
-function checkRepositoryExist() {
-}
-
-function checkBranch(branch) {
-}
-
-function checkoutBranch(branch) {
+    exec('cd ' + path, function(status, output) {
+        if (status != 0) {
+            var view = 'error';
+            renderView(res, view, output);
+        } else {
+            exec('git checkout ' + params[1], function(status, output) {
+                if (status != 0) {
+                    var view = 'error';
+                    renderView(res, view, output);
+                } else {
+                    console.log(status);
+                    console.log(output);
+                }
+            });
+        }
+    });
 }
 
 function CheckStatus() {
@@ -45,14 +77,24 @@ function CheckStatus() {
     // return true or false
 }
 
-function renderView(message, res) {
-    res.render('index.twig', {
+function renderView(res, view, message) {
+    console.log(message);
+    res.render(view + '.twig', {
         message : message
     });
 }
 
 
 
-var port = '8080';
-app.listen(port);
-console.log('application loaded and listen on port:' + port + '\n');
+
+
+
+
+
+
+
+
+
+
+app.listen(listenPort);
+console.log('application loaded and listen on port:' + listenPort + '\n');
