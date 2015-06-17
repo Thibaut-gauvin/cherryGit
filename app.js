f53c25f57d25863863a0e1c7b5ecaa20e292cca6
gauvin <gauvin.thibaut83@gmail.com>
2015-06-18 01:38:13 +0200

    add api request /repo/:repo/branch/:branch/commit
    cd, git checkout and git cherry-pick work

--- app.js
+++ app.js
@@ -2,99 +2,87 @@ require('shelljs/global');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser());
 var Twig = require('twig');
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static('public'));
 
 /* application variable */
 var git_project_path = '/var/www';
 var listenPort = '8080';
 
 if (!which('git')) {
-    echo('Sorry, this script requires git');
-    exit(1);
+    var response = {'status' : 'ko', 'message' : 'Sorry, this script requires git'};
+    renderJson(res, response);
 }
 
 /* ROUTES */
 
-/*app.get('/index', function (req, res) {
-    var view = 'index';
-    renderView(res, view);
-});
-
-app.get('/postApi', function (req, res) {
-    var view = 'form';
-    renderView(res, view);
-});
-
-app.post('/postApi', function (req, res) {
-    var repositoryName = req.body.repository;
-    var branchName = req.body.branch;
-    var commitHAsh = req.body.commit;
-    var params = [repositoryName, branchName, commitHAsh];
-    goToRepository(params, res);
-});
-
-// bad Url
-app.use(function(req, res){
-    res.redirect('/index');
-});*/
-
-app.get('/repo/:repository/branch/:branch/commit', function (req, res) {
-    var repositoryName = req.params.repository;
+app.post('/repo/:repository/branch/:branch/commit', function (req, res) {
+    var pathRepository =  git_project_path + '/' + req.params.repository;
     var branchName = req.params.branch;
-    var commitHAsh = 'ea72eaa1a6a9208ba8d3a1cfbed4f2dc5b9bda94';
-    var params = [repositoryName, branchName, commitHAsh];
+    var commitHAsh = req.body.commit;
+
+    console.log('commitHAsh: ' + commitHAsh);
+
+    var params = [pathRepository, branchName, commitHAsh];
+
     goToRepository(params, res);
 });
+
+// bad url
+app.use(function(req, res) {
+    renderJson(res, 'Bad Request');
+});
+
 
 /* Function */
 
 function goToRepository(params, res) {
-    var path =  git_project_path + '/' + params[0];
-    console.log('cd ' + path + '\n');
-
-    exec('cd ' + path, function(status, output) {
-        if (status != 0) {
-            var view = 'error';
-            renderView(res, view, output);
-        } else {
-            exec('git checkout ' + params[1], function(status, output) {
-                if (status != 0) {
-                    var view = 'error';
-                    renderView(res, view, output);
-                } else {
-                    console.log(status);
-                    console.log(output);
-                }
-            });
-        }
-    });
+    console.log('cd to ' + params[0]);
+    if (exec('cd ' + params[0]).code !== 0) {
+        var response = {'status' : 'ko', 'message' : 'cd to directory ' + params[0] + ' failed'};
+        renderJson(res, response);
+    } else {
+        checkoutBranch(params, res);
+    }
 }
 
-function CheckStatus() {
-    // check si l'on a le droit de faire le merge ou pas
-    // return true or false
+function checkoutBranch(params, res) {
+    console.log('git checkout ' + params[1]);
+    if (exec('cd ' + params[0] + ' && git checkout ' + params[1]).code !== 0) {
+        var response = {'status' : 'ko', 'message' : 'checkout branch ' + params[1] + ' failed'};
+        renderJson(res, response);
+    } else {
+        gitCherryPick(params, res);
+    }
+}
+
+function gitCherryPick(params, res) {
+    console.log('git cherry-pick ' + params[2]);
+    if (exec('cd ' + params[0] + ' && git checkout ' + params[1] + ' && git cherry-pick ' + params[2]).code !== 0) {
+        var response = {'status' : 'ko', 'message' : 'git cherry-pick \'' + params[2] + '\' failed'};
+        renderJson(res, response);
+    } else {
+        renderJson(res, 'tous va bien mec');
+    }
 }
 
 function renderView(res, view, message) {
-    console.log(message);
     res.render(view + '.twig', {
         message : message
     });
 }
 
+function renderJson(res, data) {
+    var apiResponse = {'data' : data};
+
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(JSON.stringify(apiResponse));
+    res.end();
+}
 
 
-
-
-
-
-
-
-
-
-
+/* Start application */
 
 app.listen(listenPort);
 console.log('application loaded and listen on port:' + listenPort + '\n');
